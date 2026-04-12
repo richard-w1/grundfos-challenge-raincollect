9ddb3f6b37c3dad910091f346bd55ebf142eaafb
Richard <richardwang1337@gmail.com>
2026-04-12 10:59:56 -0500

    removed files

--- TowerScout/hosting/connect.sh
+++
@@ -1,2 +0,0 @@
-ssh -L localhost:8888:localhost:8888 -i towerscout.pem ubuntu@ec2-44-230-243-242.us-west-2.compute.amazonaws.com
-
--- TowerScout/hosting/service.sh
+++
@@ -1,5 +0,0 @@
-sudo cp towerscout.service /etc/systemd/system
-sudo systemctl daemon-reload
-sudo systemctl start towerscout
-sudo systemctl enable towerscout
-
